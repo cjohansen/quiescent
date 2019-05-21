2063f346735a0bad62b54c7327d658e7bde34e2b
Christian Johansen <christian@cjohansen.no>
2019-05-21 17:17:05 +0200

    Add function to create React components with React 16 class API
    Heavily informed by this wonderful gist: https://gist.github.com/pesterhazy/2a25c82db0519a28e415b40481f84554

--- src/quiescent/factory.cljs
+++ src/quiescent/factory.cljs
@@ -1,6 +1,23 @@
 (ns quiescent.factory
   (:require-macros [quiescent.factory :as f])
-  (:require [cljsjs.react]))
+  (:require [cljsjs.react]
+            [goog.object :as gobj]))
+
+(defn create-react-class [display-name impl]
+  (let [cmp (fn [props context updater]
+              (cljs.core/this-as this
+                (js/React.Component.call this props context updater)
+                this))]
+    (gobj/extend (.-prototype cmp) js/React.Component.prototype (clj->js impl))
+
+    (when display-name
+      (set! (.-displayName cmp) display-name)
+      (set! (.-cljs$lang$ctorStr cmp) display-name)
+      (set! (.-cljs$lang$ctorPrWriter cmp)
+            (fn [this writer opt]
+              (cljs.core/-write writer display-name))))
+    (set! (.-cljs$lang$type cmp) true)
+    (set! (.. cmp -prototype -constructor) cmp)))
 
 (defn factory
   "Return a Component factory function. The argument may be any
